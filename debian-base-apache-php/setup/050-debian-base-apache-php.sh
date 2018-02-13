8ff5763f7a7b303706d45751fa15a2a32dbc8529
John Goerzen <jgoerzen@complete.org>
2018-02-13 04:13:43 -0600

    Complete the scriptifying of stretch

---
+++ debian-base-apache-php/setup/050-debian-base-apache-php.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -x
+
+mv /usr/sbin/policy-rc.d.disabled /usr/sbin/policy-rc.d
+
+apt-get update
+apt-get -y --no-install-recommends install libapache2-mod-php
+apt-get -y -u dist-upgrade
+
+apt-get clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+/usr/local/bin/docker-wipelogs
+
+mv /usr/sbin/policy-rc.d /usr/sbin/policy-rc.d.disabled
---
+++ debian-base-apache/setup/040-debian-base-apache.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -x
+
+mv /usr/sbin/policy-rc.d.disabled /usr/sbin/policy-rc.d
+# Install python-ndg-httpsclient per #861513
+apt-get update
+apt-get -y --no-install-recommends install apache2 certbot python-certbot-apache python-ndg-httpsclient ssl-cert \
+             acme-tiny
+apt-get -y -u dist-upgrade
+
+apt-get clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+/usr/local/bin/docker-wipelogs
+
+mv /usr/sbin/policy-rc.d /usr/sbin/policy-rc.d.disabled
